00261ae6b349243b1ee611f79ab7dfa58e524634
MMeghana <63573802+MMeghana@users.noreply.github.com>
2020-09-10 15:22:34 +0530

    Add files via upload

---
+++ Pronic.java
@@ -0,0 +1,21 @@
+package Prgs5;
+import java.util.*;
+public class Pronic {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter number :");
+	int num = sc.nextInt(); // num= 20
+	int x = 0;
+	for(int i=0 ; i<num; i++)  //  i = 4
+	{
+		if(i*(i+1)== num) //  20  == 20 -> t
+		{
+			x = 1;
+			break;
+		}
+	}
+	if(x ==1 )
+		System.out.println("Pronic Number");
+	else
+		System.out.println("Not a Pronic Number");
+	} }
